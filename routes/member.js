66c2e70709e9fe7a960ef11f0d207b3f05161d2b
JuliusAlphonso <JadeMaveric@users.noreply.github.com>
2019-10-27 22:36:58 +0530

    Allow new members to be added

--- routes/member.js
+++ routes/member.js
@@ -1,11 +1,43 @@
 const express = require('express');
 const memberController = require('../controller/member');
+const { check, validationResult } = require('express-validator');
+const db = require('../data/database')
 
 const router = express.Router();
 
-
-
 router.get('/members',memberController.getAllMembers);
-router.get('/members/:id',memberController.getMember)
 
-module.exports =router 
+// SHOW ADD USER FORM
+router.get('/members/add', (req, res, next) => {    
+    res.render('members-add', {
+        title: 'Add New Member',
+        first_name: '',
+        last_name: '',
+        roll_no: '',
+        instrument: ''
+    })
+})
+
+// ADD NEW MEMBER POST ACTION
+router.post('/members/add', (req, res, next) => {
+    
+    console.log(req['body']);
+
+    var member = {
+        first_name: req['body'].first_name,
+        last_name: req['body'].last_name,
+        roll_no: req['body'].roll_no,
+        instrument: req['body'].instrument || "None"
+    }
+        
+    console.log("Inserting...")
+    db.query('INSERT INTO members SET ?', member).then( (err, result) => {
+        console.log("Sucess!")
+        res.redirect(`/members/${member.roll_no}`);
+    })
+    // res.redirect(`/members`);
+})
+
+router.get('/members/:id',memberController.getMember);
+
+module.exports = router;
