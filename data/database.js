48797cc88f31fc0d7df01119d12a294ae57caea5
JuliusAlphonso <JadeMaveric@users.noreply.github.com>
2019-10-27 19:39:05 +0530

    reset root password

--- data/database.js
+++ data/database.js
@@ -4,7 +4,7 @@ const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
     database: 'audb',
-    password: 'admin'
+    password: 'root'
 });
 
 module.exports = pool.promise();
